84cafaee82bbb1c11e03370d02fae4b7eed1fb24
Nikita <ussnikita209@mail.ru>
2021-08-08 20:31:04 +0300

    feat: add processing for numbers less than twelve

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 module.exports = function toReadable (number) {
+    let numStrings = ['zero', 'one', 'two', 'three', 'four',
+     'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+      'twelve', ]
 
+    if (number <= 12) {
+      return numStrings[number]
+    }
+
+    
    return number
 }
