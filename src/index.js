fc34a46ad4338fafbcc5658fd9ed61f1dd11624a
Nikita <ussnikita209@mail.ru>
2021-08-09 10:46:02 +0300

    feat: add processing for three-digit numbers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-module.exports = function toReadable (number) {
-    let numStrings = ['zero', 'one', 'two', 'three', 'four',
-     'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
-      'twelve', ]
-
-    if (number <= 12) {
-      return numStrings[number]
-    }
-
+module.exports = function toReadable(number) {
+  let numStrings = ['zero', 'one', 'two', 'three', 'four',
+    'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+    'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+    'seventeen', 'eighteen', 'nineteen']
+  let numDozensStrings = ['twenty', 'thirty', 'forty',
+    'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
     
-   return number
+  let numString = ''
+  let hundred = +(number / 100) | 0
+  if (hundred) numString += numStrings[hundred] + ' hundred'
+
+  let dozens = number % 100 | 0
+  if (dozens <= 19) {
+    if (dozens != 0 || number == 0) {
+      numString += ' ' + numStrings[dozens]
+    }
+  } else {
+    numString += ' ' + numDozensStrings[Math.trunc(dozens / 10) - 2]
+    if (dozens % 10) numString += ' ' + numStrings[dozens % 10]
+  }
+
+  return numString.trimLeft()
 }
+
